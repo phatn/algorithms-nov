9f51cd8e0abaff8b283424085b9e8b5e1f70d6f9
Win Win Htike <winwinhtike23@gmail.com>
2022-11-02 16:57:25 -0500

    Algorithm 3,4

---
+++ W1D1/src/main/java/edu/miu/algorithms/Algorithm3.java
@@ -0,0 +1,34 @@
+package edu.miu.algorithms;
+
+public class Algorithm3 {
+    //Use one loop. Find max and min of even integers. Compute max – min.
+    public static int algorithm3(int[] randomArray){
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
+        for (int i=0; i<randomArray.length; i++){
+            if(randomArray[i]%2==0) {
+                if(max<randomArray[i])
+                    max=randomArray[i];
+                if(min>randomArray[i])
+                    min=randomArray[i];
+            }
+        }
+
+        int maxDistance=max-min;
+        return maxDistance;
+    }
+
+    public static void main(String[] args) {
+
+        int[] sizes = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+        for(int size : sizes) {
+            int[] arr = Utils.generateArray(size);
+            long startTime = System.nanoTime();
+            int largestDistance = algorithm3(arr);
+            long endTime = System.nanoTime();
+            System.out.printf("The largest distance of array size %d is  %d, took %d ms", arr.length, largestDistance, (endTime - startTime)/1000000);
+            System.out.println();
+        }
+    }
+}
---
+++ W1D1/src/main/java/edu/miu/algorithms/Algorithm4.java
@@ -0,0 +1,27 @@
+package edu.miu.algorithms;
+
+import java.util.Arrays;
+
+public class Algorithm4 {
+
+    //Use Streams to find the max and min. Compute max – min
+    public static int algorithm4(int[] randomAry){
+        int max= Arrays.stream(randomAry).filter(x-> x%2 ==0).max().getAsInt();
+        int min=Arrays.stream(randomAry).filter(x->x%2==0).min().getAsInt();
+
+        int maxDistance=max-min;
+        return maxDistance;
+    }
+
+    public static void main(String[] args) {
+        int[] sizes = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+        for(int size : sizes) {
+            int[] arr = Utils.generateArray(size);
+            long startTime = System.nanoTime();
+            int largestDistance = algorithm4(arr);
+            long endTime = System.nanoTime();
+            System.out.printf("The largest distance of array size %d is  %d, took %d ms", arr.length, largestDistance, (endTime - startTime)/1000000);
+            System.out.println();
+        }
+    }
+}
